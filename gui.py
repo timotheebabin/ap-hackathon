150d2993afe37ea62b0bb53f26871a84ab56219b
Lucile Jogee <lucile.jogee@etu.minesparis.psl.eu>
2025-01-31 16:00:12 +0100

    modifications esthétiques

--- gui.py
+++ gui.py
@@ -4,35 +4,32 @@ import numpy as np
 
 #Cette fonction renvoie un bool indiquant si le board est atteignable.
 def is_reachable(given_board):
-    assert type(given_board)==np.ndarray or type(given_board)==list, "il faut un tableau numpy ou une liste"
+    assert type(given_board) == np.ndarray or type(given_board) == list, "il faut un tableau numpy ou une liste"
     #on commence par convertir tout cela en un tableau array linéaire de 8 cases
     board = np.array([0 for i in range(8)])
-    k=0
-    given_board2=given_board.copy() #pour éviter les effets colatéraux
-    if type(given_board2)==np.ndarray and given_board2.shape==(3,3) :
+    k = 0
+    given_board2 = given_board.copy() #pour éviter les effets colatéraux
+    if type(given_board2) == np.ndarray and given_board2.shape == (3,3) :
         given_board2.reshape(9)
-    elif type(given_board2)==list :
-        given_board2=np.array(given_board2)
+    elif type(given_board2) == list :
+        given_board2 = np.array(given_board2)
     for i in range(9):
-        a=given_board2[i]
-        if a!=0:
-            board[k]=a
-            k+=1
+        a = given_board2[i]
+        if a != 0:
+            board[k] = a
+            k += 1
     
     #maintenant, on calcule la signature
     #Pour épargner au code des calculs sur des floats, on préfère faire des batteries de test.
-    epsilon=1
-    for i in range(1,8):
-        for j in range(i+1,9):
-            sigmai=board[i-1]
-            sigmaj=board[j-1]
+    epsilon = 1
+    for i in range(1, 8) :
+        for j in range(i+1, 9) :
+            sigmai = board[i-1]
+            sigmaj = board[j-1]
             if ((i<j and sigmai>sigmaj) or (i>j and sigmai<sigmaj)):
-                epsilon*=(-1)
+                epsilon *= (-1)
     #On conclut
-    if epsilon==1:
-        return True
-    else :
-        return False
+    return epsilon == 1
 
 class Board:
     def __init__(self, page):
